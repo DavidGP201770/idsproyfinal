0a6edd69c47b30e5b7bee36f791e23cf4a9435a0
DavidGP201770 <davicho201170@gmail.com>
2018-04-14 01:41:57 -0500

    xd

---
+++ src/main/java/idsfinal/org/idsproy/IdsproyApplication.java
@@ -0,0 +1,12 @@
+package idsfinal.org.idsproy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsproyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdsproyApplication.class, args);
+	}
+}
---
+++ src/test/java/idsfinal/org/idsproy/IdsproyApplicationTests.java
@@ -0,0 +1,16 @@
+package idsfinal.org.idsproy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdsproyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
